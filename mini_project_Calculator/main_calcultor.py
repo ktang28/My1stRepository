90a198a71779c180fc0c0ad027ca7fc76495ec85
ktang28 <90883302+ktang28@users.noreply.github.com>
2024-05-07 19:04:30 -0400

    Add files via upload

---
+++ mini_project_Calculator/main_calcultor.py
@@ -0,0 +1,13 @@
+first_number = input ("Please type the first number: ")
+second_number = input ("Please type the second number: ")
+a=int(first_number)
+b=int(second_number)
+sum = a+b
+subtraction=a-b
+multiplication = a*b
+division=a/b
+print("The sum of two input numbers is: ",sum)
+print("The subtraction of two input numbers is: ",subtraction)
+print("The multiplication of two input numbers is: ",multiplication)
+print("The division of two input numbers is: ",division)
+
