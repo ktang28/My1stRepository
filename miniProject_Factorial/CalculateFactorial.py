344633efbbb0221fdf695ed3d2dcb9c51e816f50
ktang28 <90883302+ktang28@users.noreply.github.com>
2024-05-09 10:34:21 -0400

    Add files via upload

---
+++ miniProject_Factorial/CalculateFactorial.py
@@ -0,0 +1,11 @@
+num = int(input("Please enter a number:"))
+factorial = 1
+if num <0:
+    print("Factorial does not exist")
+elif num == 0:
+    print("factorial is 1")
+else:
+    for i in range(1,num+1):
+        factorial = factorial * i
+    print("The factorial of", num, "is", factorial)
+
