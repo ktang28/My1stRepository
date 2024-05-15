9cd32fe34429bad2cd7275e202ae838a2896282d
ktang28 <90883302+ktang28@users.noreply.github.com>
2024-05-15 11:25:57 -0400

    Add files via upload

--- PersonalDiary/PersonalDiaryApp.py
+++ PersonalDiary/PersonalDiaryApp.py
@@ -1,15 +1,19 @@
 import os
+from datetime import datetime
 
 with open("myDiary.txt") as my_file:
     entries = my_file.read()
 while True:
 
-    print("1 - add an entry, 2 - read entries, 0 - quit:")
+    print("1 - add an entry, 2 - read previous entries, 0 - quit:")
     operation = input("Please enter your selection:")
     if operation == "1":
+        dt = datetime.now()
         content = input("Please type diary entry: ")
         with open("myDiary.txt","a") as diary:
+            diary.write(str(dt) +"\n")
             diary.write(f"{content}"+"\n")
+
         print("Diary saved\n")
     elif operation == "2":
         print("Entries: ")
